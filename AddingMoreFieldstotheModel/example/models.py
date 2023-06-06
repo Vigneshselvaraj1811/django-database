d578de0ccf20a0d198c6d1714fc9d0e49ef4d6a1
Vigneshselvaraj1811 <vigneshaasiga2020@gmail.com>
2023-06-06 12:00:00 +0530

    Add AddingMoreFieldstotheModel content

---
+++ AddingMoreFieldstotheModel/example/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
